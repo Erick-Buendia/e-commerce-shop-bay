68df28600214b4a6461e129fe79a28b552cad13e
Erick-Buendia <ericandresbuendiarosas@gmail.com>
2024-05-23 13:20:44 -0600

    retorno end point para eliminar productos

--- src/main/java/com/erick_buendia/spring/app/ecommerceshopbay/controller/ProductsController.java
+++ src/main/java/com/erick_buendia/spring/app/ecommerceshopbay/controller/ProductsController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,7 +50,6 @@ public class ProductsController {
     @DeleteMapping("/{id}")
     public Optional<Products> deleteProduct(@PathVariable Long id) {
         return productsService.delete(id);
-        //   return ResponseEntity.ok(addressUsersService.delete(id));
     }
     
 }
