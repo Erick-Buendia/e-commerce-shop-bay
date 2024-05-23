828f6099ed8c8233929c1f2dcad896224560c53d
Erick-Buendia <ericandresbuendiarosas@gmail.com>
2024-05-23 14:16:09 -0600

    Consulta para obtener los productos por id de producto

--- src/main/java/com/erick_buendia/spring/app/ecommerceshopbay/repository/ProductsRepository.java
+++ src/main/java/com/erick_buendia/spring/app/ecommerceshopbay/repository/ProductsRepository.java
@@ -1,13 +1,14 @@
 package com.erick_buendia.spring.app.ecommerceshopbay.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.erick_buendia.spring.app.ecommerceshopbay.entity.Products;
 
+public interface ProductsRepository extends CrudRepository<Products, Long> {
 
-
-public interface ProductsRepository extends CrudRepository<Products, Long>{
-
-   
-
+    @Query("SELECT p FROM Products p WHERE p.id=?1")
+    Optional<Products> findByProductsId(Long id);
 }
