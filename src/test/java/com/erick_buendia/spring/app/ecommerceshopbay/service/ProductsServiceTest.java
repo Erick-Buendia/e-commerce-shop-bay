306e386c8f3ebae4b7b9c121782fa412566b7f9d
Erick-Buendia <ericandresbuendiarosas@gmail.com>
2024-05-27 22:45:37 -0600

    Test para capa de service en productos

---
+++ src/test/java/com/erick_buendia/spring/app/ecommerceshopbay/service/ProductsServiceTest.java
@@ -0,0 +1,31 @@
+package com.erick_buendia.spring.app.ecommerceshopbay.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductsServiceTest {
+   
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+    }
+
+    @Test
+    void testSave() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+   
+    @Test
+    void testDelete() {
+
+    }
+}
